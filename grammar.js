149dbc8b5ca4863e9f17a5f532110c30e3836bbf
dorgnarg <demoor12@asu.edu>
2021-01-31 02:03:19 -0700

    Hid some nodes that didn't seem super necessary; added all the unicode values for $.name_char. Also swapped out the fragment notation that I nicked from the html grammar for a general grammar notation that's more in line with the XML specification.

--- grammar.js
+++ grammar.js
@@ -1,39 +1,36 @@
 module.exports = grammar({
     name: 'xml',
 
-    rules: {
-        fragment: $ => seq(
-            repeat($._node),
-        ),
+    extras: $ => [],
 
-        _node: $ => choice(
-            $.comment,
-            $.processing_instructions,
-            $.cdata_sect,
-            $.xml_decl,
-            $.doctype_decl,
-            $.start_tag,
-            $.end_tag,
-            $.empty_elem_tag,
-            $.element_decl,
-            $.conditional_sect,
-            $.entity_decl,
-            $.text_decl,
-            $.notation_decl,
-            $.char_data,
-            // $.text,
+    rules: {
+        document: $ => seq(
+            repeat($.prolog),
+            repeat($.element),
+            repeat($.misc)
         ),
 
         /** Names and Tokens **/
 
         _name_char: $ => choice (
             /\w/,
-            /\d/,
             '.',
             '-',
-            '_',
             ':',
-            // TODO: CombiningChar, _Extender
+            /[\u0300-\u0345\u0360-\u0361\u0483-\u0486\u0591-\u05A1\u05A3-\u05B9\u05BB-\u05BD]/,
+            /[\u05BF\u05C4\u0670\u093C\u094D\u09BC\u09BE\u09BF\u09D7\u0A02\u0A3C\u0A3E\u0A3F]/,
+            /[\u05C1-\u05C2\u064B-\u0652\u06D6-\u06DC\u06DD-\u06DF\u06E0-\u06E4\u06E7-\u06E8]/,
+            /[\u06EA-\u06ED\u0901-\u0903\u093E-\u094C\u0951-\u0954\u0962-\u0963\u0981-\u0983]/,
+            /[\u09C0-\u09C4\u09C7-\u09C8\u09CB-\u09CD\u09E2-\u09E3\u0A40-\u0A42\u0A47-\u0A48]/,
+            /[\u0A4B-\u0A4D\u0A70-\u0A71\u0A81-\u0A83\u0ABC\u0ABE-\u0AC5\u0AC7-\u0AC9\u3005]/,
+            /[\u0ACB-\u0ACD\u0B01-\u0B03\u0B3C\u309A\u00B7\u02D0\u02D1\u0387\u0640\u0E46\u0EC6]/,
+            /[\u0B3E-\u0B43\u0B47-\u0B48\u0B4B-\u0B4D\u0B56-\u0B57\u0B82-\u0B83\u0BBE-\u0BC2]/,
+            /[\u0BC6-\u0BC8\u0BCA-\u0BCD\u0BD7\u0D46-\u0D48\u0D4A-\u0D4D\u0D57\u0E31]/,
+            /[\u0C01-\u0C03\u0C3E-\u0C44\u0C46-\u0C48\u0C4A-\u0C4D\u0C55-\u0C56\u0C82-\u0C83]/,
+            /[\u0CBE-\u0CC4\u0CC6-\u0CC8\u0CCA-\u0CCD\u0CD5-\u0CD6\u0D02-\u0D03\u0D3E-\u0D43]/,
+            /[\u0E34-\u0E3A\u0E47-\u0E4E\u0EB1\u0EB4-\u0EB9\u0EBB-\u0EBC\u0EC8-\u0ECD\u0F18-\u0F19]/,
+            /[\u0F35\u0F37\u0F39\u0F3E\u0F3F\u0F71-\u0F84\u0F86-\u0F8B\u0F90-\u0F95\u0F97]/,
+            /[\u0F99-\u0FAD\u0FB1-\u0FB7\u0FB9\u20D0-\u20DC\u20E1\u302A-\u302F\u3099]/,
         ),
 
         _name: $ => seq (
@@ -133,7 +130,6 @@ module.exports = grammar({
 
         // char_data: $ => /[^<&]* - ([^<&]* ']]>' [^<&]*)/,
         char_data: $ => /[^<&]*/,
-        // text: $ => /[^<>]+/,
 
         /** Comments **/
 
@@ -165,25 +161,36 @@ module.exports = grammar({
         /** CDATA Sections **/
 
         cdata_sect: $ => seq(
-            $.cdata_start,
+            $._cdata_start,
             $.cdata,
-            $.cdata_end
+            $._cdata_end
         ),
 
-        cdata_start: $ => '<![CDATA[',
+        _cdata_start: $ => '<![CDATA[',
 
         cdata: $ => /./,
 
-        cdata_end: $ => ']]>',
+        _cdata_end: $ => ']]>',
 
         /** Prolog **/
 
-        prolog: $ => seq(
-            optional($.xml_decl),
-            repeat($.misc),
-            optional($.doctype_decl),
-            repeat($.misc)
-        ),
+        prolog: $ => prec.right(choice(
+            seq(
+                $.xml_decl,
+                optional($.misc),
+            ),
+            seq(
+                $.xml_decl,
+                optional($.misc),
+                $.doctype_decl,
+                optional($.misc)
+            ),
+            seq(
+                optional($.misc),
+                $.doctype_decl,
+                optional($.misc)
+            ),
+        )),
 
         xml_decl: $ => seq (
             '<?xml',
@@ -217,7 +224,7 @@ module.exports = grammar({
         _eq: $ => seq(
             optional(/\s/),
             '=',
-            optional(/\s/)
+            optional(/\s/),
         ),
 
         version_num: $ => repeat1( choice(
@@ -236,24 +243,24 @@ module.exports = grammar({
         doctype_decl: $ => seq (
             '<!DOCTYPE',
             /\s/,
-            // alias($._name,$.doctype),
-            $._name,
+            alias($._name,$.doctype),
+            // $._name,
             optional(seq(/\s/, $.external_id)),
             optional(/\s/),
             optional(seq(
                 '[',
                 repeat(choice(
-                    $.markup_decl,
+                    $._markup_decl,
                     $.pe_reference,
                     /\s/,
                 )),
-                ']'
+                ']',
+                optional(/\s/),
             )),
-            // TODO: The rest of this
             '>'
         ),
 
-        markup_decl: $ => choice(
+        _markup_decl: $ => choice(
             $.element_decl,
             $.attlist_decl,
             $.entity_decl,
@@ -270,7 +277,7 @@ module.exports = grammar({
         ),
 
         external_subset_decl: $ => choice(
-            $.markup_decl,
+            $._markup_decl,
             $.conditional_sect,
             $.pe_reference
         ),
@@ -313,12 +320,12 @@ module.exports = grammar({
         ),
 
         langcode: $ => choice(
-            $.iso639_code,
-            $.iana_code,
-            $.user_code
+            $._iso639_code,
+            $._iana_code,
+            $._user_code
         ),
 
-        iso639_code: $ => seq(
+        _iso639_code: $ => seq(
             choice(
                 /[a-z]/,
                 /[A-Z]/,
@@ -329,7 +336,7 @@ module.exports = grammar({
             ),
         ),
 
-        iana_code: $ => seq(
+        _iana_code: $ => seq(
             choice(
                 'i',
                 'I'
@@ -343,7 +350,7 @@ module.exports = grammar({
             )
         ),
 
-        user_code: $ => seq(
+        _user_code: $ => seq(
             choice(
                 'x',
                 'X'
@@ -370,7 +377,7 @@ module.exports = grammar({
             $.empty_elem_tag,
             seq(
                 $.start_tag,
-                optional($.content),
+                optional($._content),
                 $.end_tag
             )
         ),
@@ -407,10 +414,10 @@ module.exports = grammar({
 
         /** Content of Elements **/
 
-        content: $ => repeat1 ( choice (
+        _content: $ => repeat1 ( choice (
             $.element,
             $.cdata_sect,
-            $.char_data,
+            alias($.char_data, $.text),
             $.reference,
             $.processing_instructions,
             $.comment,
@@ -550,7 +557,7 @@ module.exports = grammar({
 
         att_def: $ => seq(
             /\s/,
-            $._name,
+            alias($._name, $.attribute_name),
             /\s/,
             $.att_type,
             /\s/,
@@ -626,7 +633,7 @@ module.exports = grammar({
                 optional(
                     seq(
                         '#FIXED',
-                        optional(/\s/)
+                        optional(/\s/),
                     )
                 ),
                 $.attribute_value
@@ -793,7 +800,7 @@ module.exports = grammar({
 
         external_parsed_ent: $ => seq( 
             optional($.text_decl),
-            $.content
+            $._content
         ),
 
         external_pe: $ => seq(
@@ -812,18 +819,18 @@ module.exports = grammar({
             choice(
                 seq(
                     '"',
-                    $.enc_name,
+                    $._enc_name,
                     '"'
                 ),
                 seq(
                     "'",
-                    $.enc_name,
+                    $._enc_name,
                     "'"
                 )
             )
         ),
 
-        enc_name: $ => seq(
+        _enc_name: $ => seq(
             /[A-Za-z]/,
             repeat(
                 choice(
